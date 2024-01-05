29fe50963ec69e72751f9d8db65c1ac02f2d41a0
alexsatm <dzamilovaalbina2@gmail.com>
2024-01-05 19:17:30 +0300

    feat: add listener

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const lists = document.getElementById('list');
 const rightBox = document.getElementById('right');
 const leftBox = document.getElementById('left');
+
+for(let list of lists) {
+    list.addEventListener('dragstart', (e) => {
+        let selected = e.target;
+
+        rightBox.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+
+        rightBox.addEventListener('drop', (e) => {
+            rightBox.append(selected);
+            selected = null
+        })
+    })
+}
